7732140ddbb668e92386804459ffae8f465a2047
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-14 21:41:54 +0300

    Validate Price, it should greater than zero, equals to product price, and equals to subtotal amount

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -13,17 +13,24 @@ public class OrderItem extends BaseEntity<OrderItemId> {
     private OrderId orderId;
     private final Product product;
     private final int quantity;
-    private final Money money;
+    private final Money price;
     private final Money subTotal;
-
-    //builder constructor
 
     public OrderItem(OrderItem.OrderItemBuilder builder) {
         super.setId(builder.build().getId());
         this.orderId = builder.orderId;
         this.product = builder.product;
         this.quantity = builder.quantity;
-        this.money = builder.money;
+        this.price = builder.price;
         this.subTotal = builder.subTotal;
+    }
+
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+        this.orderId = orderId;
+        super.setId(orderItemId);
+    }
+
+    boolean isPriceValid() {
+        return price.isGreaterThanZero() && price.equals(product.getPrice()) && price.multiply(quantity).equals(subTotal);
     }
 }
