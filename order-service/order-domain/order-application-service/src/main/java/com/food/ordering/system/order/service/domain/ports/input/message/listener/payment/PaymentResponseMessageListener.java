4f08572e534430e58c801da2bc0aed83113bc5fb
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-26 19:23:00 +0300

    This interface is used to listen messages from payment service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/payment/PaymentResponseMessageListener.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.payment;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+
+
+/**
+ * This class is used to listen messages from payment service
+ * bu sınıf payment servisinden gelen mesajları dinlemek için kullanılır.
+ */
+public interface PaymentResponseMessageListener {
+    void paymentCompleted(PaymentResponse paymentResponse);
+    void paymentCancelled(PaymentResponse paymentResponse);
+}
