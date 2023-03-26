bcab671bdb6ba09f2551543114ca036cea4b7759
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-26 19:26:34 +0300

    This interface is used to publish OrderCreatedEvent to Payment Service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+
+/**
+ *This interface is used to publish OrderCreatedEvent to payment service
+ * Bu interface, OrderCreatedEvent'i payment servisine  yaymak için kullanılır
+ */
+public interface OrderCreatedPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCreatedEvent> {
+
+}
