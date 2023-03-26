e32bb84a11c32cf19d8410c66d92c636b26050ca
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-26 19:27:02 +0300

    This class is used to map data from one object to another

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain.mapper;
+
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * This class is used to map data from one object to another
+ */
+@Component
+public class OrderDataMapper {
+}
