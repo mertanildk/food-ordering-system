573220fa37517695b056973061e2ca78c8dd62d2
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-27 23:33:11 +0300

    Deleted Builder Pattern

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -4,6 +4,7 @@ import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valueobject.RestaurantId;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.util.List;
 
@@ -11,36 +12,11 @@ import java.util.List;
 @Getter
 public class Restaurant extends AggregateRoot<RestaurantId> {
     private final List<Product> products;
-    private boolean active;
+    private final boolean active;
 
-    private Restaurant(Builder builder) {
-        super.setId(builder.restaurantId);
-        products = builder.products;
-        active = builder.active;
-    }
-
-
-    public static final class Builder {
-        private RestaurantId restaurantId;
-        private final List<Product> products;
-        private boolean active;
-
-        public Builder(List<Product> products) {
-            this.products = products;
-        }
-
-        public Builder restaurantId(RestaurantId val) {
-            restaurantId = val;
-            return this;
-        }
-
-        public Builder active(boolean val) {
-            active = val;
-            return this;
-        }
-
-        public Restaurant build() {
-            return new Restaurant(this);
-        }
+    public Restaurant(RestaurantId restaurantId,List<Product> products ) {
+        super.setId(restaurantId);
+        this.products = products;
+        this.active = true;
     }
 }
