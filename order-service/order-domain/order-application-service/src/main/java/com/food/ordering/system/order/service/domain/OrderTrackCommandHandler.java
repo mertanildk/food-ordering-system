2ae271ef93b6f01e98f95b556f6dfbaf21b1533e
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-28 18:04:55 +0300

    TrackOrder method is pulling order by track id and throw OrderNotFoundException

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -2,12 +2,33 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OrderTrackCommandHandler {
+
+    private final OrderRepository orderRepository;
+    private final OrderDataMapper orderDataMapper;
+
+    @Transactional(readOnly = true)//
     public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        Optional<Order> orderOptional = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getTrackingId()));
+        if (orderOptional.isEmpty()) {
+            log.warn("Order is not found with tracking id: {}", trackOrderQuery.getTrackingId());
+            throw new OrderNotFoundException("Order is not found with tracking id: {}" + trackOrderQuery.getTrackingId());
+        }
+        return orderDataMapper.orderToTrackOrderResponse(orderOptional.get());
     }
 }
