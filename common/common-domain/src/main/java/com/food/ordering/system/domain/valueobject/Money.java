e60f8b13bc3bb83ee9f338c79b389e9aaf9bcb2b
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-30 23:57:44 +0300

    added getAmount method added equals and hashcode method

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -2,22 +2,23 @@ package com.food.ordering.system.domain.valueobject;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Objects;
 
 public class Money {
-
-    public static final Money ZERO = new Money(BigDecimal.ZERO);//set amount 0
     private final BigDecimal amount;
+
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
 
     public Money(BigDecimal amount) {
         this.amount = amount;
     }
 
     public boolean isGreaterThanZero() {
-        return isNotNull() && this.amount.compareTo(BigDecimal.ZERO) > 0;
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
     }
 
     public boolean isGreaterThan(Money money) {
-        return isNotNull() && this.amount.compareTo(money.getAmount()) > 0;
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
     }
 
     public Money add(Money money) {
@@ -29,20 +30,27 @@ public class Money {
     }
 
     public Money multiply(int multiplier) {
-        return new Money(setScale(this.amount.multiply(BigDecimal.valueOf(multiplier))));
-
-    }
-
-
-    private BigDecimal setScale(BigDecimal input) {// it will return bkz 1.4 -> 1 , 1.5 -> 2 , 1.6 -> 2 1.11111111 -> 1.11
-        return input.setScale(2, RoundingMode.HALF_EVEN);//kümülatif olarak 1.11111111 -> 1.11
-    }
-
-    private boolean isNotNull() {
-        return this.amount != null;
+        return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
     }
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount.equals(money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    private BigDecimal setScale(BigDecimal input) {// it will return bkz 1.4 -> 1 , 1.5 -> 2 , 1.6 -> 2 1.11111111 -> 1.11
+        return input.setScale(2, RoundingMode.HALF_EVEN);//kümülatif olarak 1.11111111 -> 1.11
     }
 }
